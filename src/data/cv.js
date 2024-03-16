07b0367a0bae31b85957c619127b620c27106628
Quang Huy <85470086+QuangHuyXX@users.noreply.github.com>
2024-03-16 13:34:03 +0700

    Update cv link

--- src/data/cv.js
+++ src/data/cv.js
@@ -1,6 +1,6 @@
 const cvLink = [
     {
-        link: "https://docs.google.com/uc?export=download&id=1RHM0AHlG9REOKWJkxQ-IS8Q0xnO_t7z7"
+        link: "https://drive.google.com/uc?export=download&id=1RHM0AHlG9REOKWJkxQ-IS8Q0xnO_t7z7"
     }
 ]
 export default cvLink;
